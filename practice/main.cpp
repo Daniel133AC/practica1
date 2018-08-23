7536444ff22dfd9b0bc9bb58650bc73b1b99ab4e
ALUMNO-N415 <ALUMNO-N415@UCSP.AULAB>
2018-08-23 09:48:57 -0500

    adicion dos numeros

--- practice/main.cpp
+++ practice/main.cpp
@@ -4,6 +4,26 @@ using namespace std;
 
 int main()
 {
+    int a,b,c,d,e;
+    cout <<"Ingresa el primer numero: ";
+    cin>>a;
+    cout <<"Ingresa el segundo numero: ";
+    cin>>b;
+    cout <<"Ingresa el tercer numero: ";
+    cin>>c;
+    cout <<"Ingresa el cuarto numero: ";
+    cin>>d;
+    cout <<"Ingresa el quinto numero: ";
+    cin>>e;
+    cout<<endl;
+    cout <<"El promedio total es: "<<(a+b+c+d+e)/5.0<<endl;
     cout << "HOLA DANIEL" << endl;
+
+    short int z =32766;
+    short int y=z+1;
+    cout<<y<<endl;
+    y=y+1;
+    cout<<y<<endl;
+
     return 0;
 }
